139e6758ff3b9fe5de052ec4cb427c8e9e1ec340
ziel5122 <azielinski@csumb.edu>
2016-05-04 15:37:34 -0700

    pushing encapsulated classifier

--- classifier.py
+++ classifier.py
@@ -2,34 +2,43 @@ from pymongo import MongoClient
 import random
 import nltk
 
-binary = False
+def getDB():
+    client = MongoClient('argon.plttn.me', 27017)
+    db = client['hashtag-pulse']
+    db.authenticate('pulseUser', '+xCh4VduYDX1cG')
+    return db
 
-client = MongoClient('argon.plttn.me', 27017)
-db = client['hashtag-pulse']
-db.authenticate('pulseUser', '+xCh4VduYDX1cG')
-
-cursor = db.tweets.find({'$or':[{'label':"happy/excited"},{'label':"joke"},{'label':"sad/disappointed"},{'label':"angry"}]},{'text':1,'label':1,'_id':0})
-
-tweets = []
-
-count = 0
-for tweet in cursor:
-    item = []
-    item.append([word.lower() for word in tweet['text'].split() if len(word) >= 3])
-    if binary:
-        if (tweet['label'] == 1 or tweet['label'] == 2):
-            item.append(1)
+def buildTweetClassifier(binary):
+    db = getDB()
+    cursor = db.tweets.find({'$or':[{'label':"happy/excited"},{'label':"joke"},{'label':"sad/disappointed"},{'label':"angry"}]},{'text':1,'label':1,'_id':0})
+    
+    tweets = []
+    for tweet in cursor:
+        item = []
+        item.append([word.lower() for word in tweet['text'].split() if len(word) >= 3])
+        if binary:
+            if (tweet['label'] == 1 or tweet['label'] == 2):
+                item.append(1)
+            else:
+                item.append(3)
         else:
-            item.append(3)
-    else:
-        item.append(tweet['label'])
-    tweets.append(item)
-    count += 1
+            item.append(tweet['label'])
+        tweets.append(item)
+        
+    word_features = get_word_features(get_words_in_tweets(tweets))
+    
+    def extract_features(document):
+        document_words = set(document)
+        features = {}
+        for word in word_features:
+            features['contains(%s)' % word] = (word in document_words)
+        return features
+    
+    training_set = nltk.classify.apply_features(extract_features, tweets)
 
-print len(tweets)
-print count
-
-print tweets[0:15]
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    
+    return classifier
 
 def get_words_in_tweets(tweets):
     all_words = []
@@ -42,20 +51,12 @@ def get_word_features(wordlist):
     word_features = wordlist.keys()
     return word_features
 
-def extract_features(document):
-    document_words = set(document)
-    features = {}
-    for word in word_features:
-        features['contains(%s)' % word] = (word in document_words)
-    return features
 
-word_features = get_word_features(get_words_in_tweets(tweets))
-print word_features[0]
 
-training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = buildTweetClassifier(True)
+print classifier
 
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-
+'''
 num_right = 0
 for i in range(0, len(tweets)):
     predicted = classifier.classify(extract_features(tweets[i][0]))
@@ -71,7 +72,7 @@ classifier.show_most_informative_features(10)
 #tweets[i][0] is a list of tokens
 #tweets[i][1] is the label
 
-'''
+
 while (cursor.hasNext()):
     tweet = cursor.next()
     print tweet['text']
